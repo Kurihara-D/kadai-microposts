b345007fd7a92092a1df91fb6a9b3593275829b6
kuriharadaiki <na.ur666@gmail.com>
2018-01-21 16:42:48 +0900

    make post

--- microposts/app/Http/Controllers/Controller.php
+++ microposts/app/Http/Controllers/Controller.php
@@ -9,4 +9,16 @@ use Illuminate\Foundation\Validation\ValidatesRequests;
 abstract class Controller extends BaseController
 {
     use DispatchesJobs, ValidatesRequests;
+//    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    public function counts($user)
+    {
+        $count_microposts = $user->microposts()->count();
+
+        return [
+            'count_microposts' => $count_microposts,
+        ];
+    }
 }
+
+
--- microposts/app/Http/routes.php
+++ microposts/app/Http/routes.php
@@ -10,10 +10,10 @@
 | and give it the controller to call when that URI is requested.
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'WelcomeController@index');
+//Route::get('/', function () {
+//    return view('welcome');
+//});
 
 Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
 Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
@@ -23,6 +23,8 @@ Route::get('login', 'Auth\AuthController@getLogin')->name('login.get');
 Route::post('login', 'Auth\AuthController@postLogin')->name('login.post');
 Route::get('logout', 'Auth\AuthController@getLogout')->name('logout.get');
 
+
 Route::group(['middleware' => 'auth'], function () {
     Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+    Route::resource('microposts', 'MicropostsController', ['only' => ['store', 'destroy']]);
 });
